6f93445ab7ea7d52dd64bd2f4277c5a78a507dd9
Vladislav <wwwxumuk@mail.ru>
2023-02-25 12:36:30 +0300

    no message

---
+++ Assets/Controller.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Controller : MonoBehaviour
+{
+    [SerializeField] private float _jumpForce = 5f;
+    [SerializeField] private float _speed = 1f;
+
+
+    [Header("PhisicSetting")]
+    [SerializeField] private float _minGroundNormalY = 0.65f;
+    [SerializeField] private float _gravityModifier = 1f;
+    [SerializeField] private Vector2 _velocity;
+    [SerializeField] private LayerMask _layerMask;
+
+    protected Vector2 targetVelocity;
+    protected bool grounded;
+    protected Vector2 groundNormal;
+    protected Rigidbody2D rb2d;
+    protected ContactFilter2D contactFilter;
+    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(16);
+
+    protected const float minMoveDistance = 0.001f;
+    protected const float shellRadius = 0.01f;
+
+    private void OnEnable()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        contactFilter.useTriggers = false;
+        contactFilter.SetLayerMask(_layerMask);
+        contactFilter.useLayerMask = true;
+    }
+
+    private void Update()
+    {
+        targetVelocity = new Vector2(Input.GetAxis("Horizontal"), 0);
+
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            _velocity.y = _jumpForce;
+    }
+
+    private void FixedUpdate()
+    {
+        _velocity += _gravityModifier * Physics2D.gravity * Time.deltaTime;
+        _velocity.x = targetVelocity.x;
+
+        grounded = false;
+
+        Vector2 deltaPosition = _velocity * Time.deltaTime;
+        Vector2 moveAlongGround = new Vector2(groundNormal.y, -groundNormal.x);
+        Vector2 move = moveAlongGround * deltaPosition.x * _speed;
+
+        Movement(move, false);
+
+        move = Vector2.up * deltaPosition.y;
+
+        Movement(move, true);
+    }
+
+    private void Movement(Vector2 move, bool yMovement)
+    {
+        float distance = move.magnitude;
+
+        if(distance > minMoveDistance)
+        {
+            int count = rb2d.Cast(move,contactFilter,hitBuffer,distance+shellRadius);
+
+            hitBufferList.Clear();
+
+            for(int i=0; i<count; i++)
+            {
+                hitBufferList.Add(hitBuffer[i]);
+            }
+
+            for(int i=0; i<count; i++)
+            {
+                Vector2 currentNormal = hitBufferList[i].normal;
+                if (currentNormal.y > _minGroundNormalY)
+                {
+                    grounded = true;
+                    if (yMovement)
+                    {
+                        groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+
+                float proection = Vector2.Dot(_velocity, currentNormal);
+                if(proection < 0)
+                {
+                    _velocity -= proection * currentNormal;
+                }
+
+                float modifiedDistance = hitBufferList[i].distance - shellRadius;
+                distance = modifiedDistance<distance? modifiedDistance : distance;
+            }
+        }
+
+        rb2d.position += move.normalized * distance;
+    }
+}
